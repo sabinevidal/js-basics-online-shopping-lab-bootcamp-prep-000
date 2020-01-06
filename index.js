fbfc10bee3157c5845eedb9d434f66958b92769a
Sabine Vidal <svidal@live.co.za>
2020-01-06 11:23:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function addToCart(item) {
  var newItem = { itemName: item, itemPrice: Math.floor(Math.random() * 101) };
  cart.push(newItem);
  return `${item} has been added to your cart.`;
+ console.log
 }
 
 function viewCart() {
