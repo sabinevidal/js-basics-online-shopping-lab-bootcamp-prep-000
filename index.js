709c1c209dfb91a81a5923fd60ae81a192cedbc8
Sabine Vidal <svidal@live.co.za>
2020-01-06 13:06:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function viewCart() {
     for (var i = 0; i < cart.length - 1; i++) {
       myStr = MyStr + `${cart[i].itemName} at $${cart[i].itemPrice}` 
     }
-    return myStr + `
+    return myStr + `${cart[cart.length-1].itemName} at $${cart[cart.length-1].itemPrice}`
   }
 }
 
