7e5cfb8654d6510545a3ba2ca2575580ec10441e
Sabine Vidal <svidal@live.co.za>
2020-01-06 13:19:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function viewCart() {
 
 function total() {
   // write your code here
-  total = 0;
+  var total = 0;
   for (var i = 0; i <= cart.length; i++) {
     total += cart[i].itemPrice;
   }
