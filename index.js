d7502255af2e371056afeb8c665fe6198e440ef3
Sabine Vidal <svidal@live.co.za>
2020-01-06 13:26:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,8 +46,9 @@ function removeFromCart(item) {
   // write your code here
   for (var i = 0; i < cart.length; i++) {
     if (item === cart[i].itemName) {
-      
+      cart.splice(i,1);
     }
+    return cart;
   }
  
 }
