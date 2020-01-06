ebfa96b70198500aa93b46756b31ecb81c5c3199
Sabine Vidal <svidal@live.co.za>
2020-01-06 13:54:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -35,11 +35,11 @@ function viewCart() {
 
 function total() {
   // write your code here
-  let total = 0;
+  let totalPrice = 0;
   for (var i = 0; i < cart.length; i++) {
     total += cart[i].itemPrice;
   }
-  return total;
+  return totalPrice;
 }
 
 function removeFromCart(item) {
@@ -60,6 +60,6 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
-    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`;
+    return `Your total cost is $${totalPrice}, which will be charged to the card ${cardNumber}.`;
   }
 }
