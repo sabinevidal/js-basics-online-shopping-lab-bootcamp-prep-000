61b4c4e3cd6461cdc607a628fcd84d3d1e191446
Sabine Vidal <svidal@live.co.za>
2020-01-06 13:51:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -61,6 +61,6 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
-        cart = [];
+    cart = [];
   }
 }
