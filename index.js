f1e71abfcadbcc0e91f41dfe5b0d853fa5665a0c
Sabine Vidal <svidal@live.co.za>
2020-01-06 13:39:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,6 +58,6 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if (cardNumber === undefined) {
-    return 
+    return "Sorry, we don't have a credit card on file for you."
   }
 }
