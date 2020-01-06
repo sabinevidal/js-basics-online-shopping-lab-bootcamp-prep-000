4346c4fbd53a6b92e33d08228e5fc4e1672fbe9f
Sabine Vidal <svidal@live.co.za>
2020-01-06 13:02:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ function viewCart() {
   } else if (cart.length === 2) {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
   } else {
+    var myStr = "in your cart, you have";
     
   }
 }
