bcf98dc74bd643ea679502008e8ef10e73a15d2c
Sabine Vidal <svidal@live.co.za>
2020-01-06 11:26:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function viewCart() {
   var itemsAndPrices = [];
   //iterating over cart
   for (var i=0; i < cart.length; i++){
+    var key = Object.keys(cart[i])[0];
     itemsAndPrices.push(key + " at $" + cart[i][key]);
   }
   
