383aa1b54e5877bb22a09031dc7e06678c268617
Sabine Vidal <svidal@live.co.za>
2020-01-06 11:21:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function viewCart() {
   //iterating over cart
   for (var i=0; i < cart.length; i++){
     var key = Object.keys(cart[i])[0];
-    itemsAndPrices.push(key + "at $" + cart[i][key]);
+    itemsAndPrices.push(key + " at $" + cart[i][key]);
   }
   
   var beginSentence = "In your cart, you have";
